58ab0f1375e880ec671142ce03a6bf1d365f96a8
marufboy <afif23ixa@gmail.com>
2022-03-23 08:14:32 +0800

    initial push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './Data';
+
+const albumImage = data.album.images[1].url;
+const albumTitle = data.album.name;
+const albumArtist = data.album.artists[0].name;
+
+const Album = () => (
+  <div className="newHeader">
+    <div className="headerLogo">
+      <img className="logo" src={albumImage} alt="Logo" />
+    </div>
+    <div className="headerDesc">
+      <h1>{albumTitle}</h1>
+      <p>Artist : {albumArtist}</p>
+      <button class="btnSelect">
+        Select
+      </button>
+    </div>
+  </div>
+);
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Album />
       </header>
     </div>
   );
